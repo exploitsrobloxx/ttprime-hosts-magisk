9810eeffcffde9b1bf4e9e315bf2e8c901b10be3
Simon <8466614+SimonBaars@users.noreply.github.com>
2024-11-28 21:13:02 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,70 +1,28 @@
-##########################################################################################
-#
-# MMT Extended Config Script
-#
-##########################################################################################
+#!/system/bin/sh
 
-##########################################################################################
-# Config Flags
-##########################################################################################
+# Check root implementation
+ui_print "- Checking root implementation"
+if [ "$BOOTMODE" ] && [ "$KSU" ]; then
+ui_print "- Installing from KernelSU app"
+ui_print "   KernelSU version: $KSU_KERNEL_VER_CODE (kernel) + $KSU_VER_CODE (ksud)"
+if [ "$(which magisk)" ]; then
+ui_print "   Multiple root implementation is NOT supported"
+abort    "   Aborting!"
+fi
+elif [ "$BOOTMODE" ] && [ "$MAGISK_VER_CODE" ]; then
+ui_print "- Installing from Magisk app"
+else
+ui_print "   Installation from recovery is NOT supported"
+ui_print "   Please install from Magisk / KernelSU app"
+abort    "   Aborting!"
+fi
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment PARTOVER if you have a workaround in place for extra partitions in regular magisk install (can mount them yourself - you will need to do this each boot as well). If unsure, keep commented
-# Uncomment PARTITIONS and list additional partitions you will be modifying (other than system and vendor), for example: PARTITIONS="/odm /product /system_ext"
-#MINAPI=21
-#MAXAPI=25
-#DYNLIB=true
-#PARTOVER=true
-#PARTITIONS=""
+# Patch default hosts file
+PATH=/system/etc
+ui_print "- Patching hosts file"
+mkdir -p $MODPATH$PATH
+mv -f $MODPATH/hosts $MODPATH$PATH
 
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
-REPLACE="
-"
-
-##########################################################################################
-# Permissions
-##########################################################################################
-
-set_permissions() {
-  : # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
-  
-  # For directories (includes files in them):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
-
-  # For files (not in directories taken care of above)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
-}
-
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
-
-SKIPUNZIP=1
-unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
+# Clean up
+rm -rf $MODPATH/hosts
+rm -rf $MODPATH/LICENSE
